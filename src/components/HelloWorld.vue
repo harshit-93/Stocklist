d1d546566c8aa6a256c2c1e907196bf1efe1042a
harshit-93 <58566848+harshit-93@users.noreply.github.com>
2021-11-12 19:25:07 +0530

    Add files via upload

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,41 +1,62 @@
 <template>
         <section class="main">
             <form class="search" method="post" action="index.html" >
-                <input id="search" type="text" name="q" @input="getSuggestion" placeholder="Search..." />
-                <ul class="results" >
-                    <li><a href="index.html">Search Result #1<br /></a></li>
+                <input id="search" type="text" name="q" @input="getSuggestion" placeholder="Search..." autocomplete="off" />
+                <ul class="results">
+                    <li v-for="(match, index) in bestMatches" :key="index" @click="getDesc"> {{ match['1. symbol'] }} <br /></li>
                 </ul>
             </form>
         </section>
+        <p>{{ description }}</p>
     
 </template>
 
 <script>
+/* eslint-disable eslint-disable-next-line */
+import axios from 'axios';
+
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
   },
+  data() {
+    return {
+      bestMatches: [],
+      description: ""
+    }
+  },
   methods: {
         getSuggestion(event) {
-            const results = document.querySelector('.results');
-            while (results.firstChild) {
-                results.removeChild(results.firstChild);
-            }
+            // const results = document.querySelector('.results');
+            // while (results.firstChild) {
+            //     results.removeChild(results.firstChild);
+            // }
 
-            fetch('https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=' + event.target.value + '&apikey=1L0K210ONUBKP1KR')
-            .then(response => response.json())
+            axios.get('https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=' + event.target.value + '&apikey=1L0K210ONUBKP1KR')
             .then((data) => {
                 console.log(data);
-                data.bestMatches.forEach(element => {
-                    console.log(element['1. symbol']);
-                    let li = document.createElement('li');
-                    let a = document.createElement('a');
-                    a.innerText = element['1. symbol'];
-                    li.appendChild(a);
+                this.bestMatches = data.data['bestMatches'];
+                console.log(this.bestMatches);
+                // data.bestMatches.forEach(element => {
+                //     console.log(element['1. symbol']);
+                //     let li = document.createElement('li');
+                //     let a = document.createElement('a');
+                //     a.innerText = element['1. symbol'];
+                //     li.appendChild(a);
 
-                    document.querySelector('.results').appendChild(li);
-                });
+                //     document.querySelector('.results').appendChild(li);
+                // });
+            });
+        },
+        getDesc(event){
+          console.log(event.target.innerText);
+          axios.get('https://www.alphavantage.co/query?function=OVERVIEW&symbol=' + event.target.innerText + '&apikey=1L0K210ONUBKP1KR')
+            .then((data) => {
+                console.log(data);
+                this.description = data.data['Description'];
+                console.log(this.description);
+                
             });
         }
     }
